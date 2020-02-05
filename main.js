658318ece5da007df11b7fa2ffe09542efba0171
Mstiekema <merijnstiekema@outlook.com>
2020-02-05 17:44:04 +0100

    Added semantic and created simple base html

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const { app, BrowserWindow } = require('electron');
+const ejs = require('ejs');
 
 function createWindow () {
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       nodeIntegration: true
     }
   });
-  win.loadFile('src/views/home/home.html');
+  
+  win.loadFile('src/views/base/base.html');
+  win.webContents.openDevTools()
 }
 
 app.on('ready', createWindow);
