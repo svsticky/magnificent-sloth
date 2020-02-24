f5c4d16c9794be68ad7e39ee8a596a17982ee0be
Nireon <siemvdtweel@gmail.com>
2020-02-24 11:47:12 +0100

    nfc testing

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 const { app, BrowserWindow } = require('electron');
+const { NFC } = require('nfc-pcsc');
 
+
+
+function initialize () {
+const nfc = new NFC();
+console.log("nfc start")
+nfc.on('reader', reader => {
+
+	console.log(reader.name + ' reader attached, waiting for cards ...');
+
+	reader.on('card', card => {
+		console.log(card.uid);
+	});
+
+	reader.on('error', err => {
+		console.error('reader error', err);
+	});
+
+	reader.on('end', () => {
+		console.log(reader.name + ' reader disconnected.');
+	});
+});
+
+nfc.on('error', err => {
+	console.error(err);
+});
+
+  createWindow()
+}
 function createWindow () {
   let win = new BrowserWindow({
     width: 800,
@@ -8,7 +37,8 @@ function createWindow () {
       nodeIntegration: true
     }
   });
+  win.webContents.openDevTools();
   win.loadFile('src/views/home/home.html');
 }
 
-app.on('ready', createWindow);
+app.on('ready', initialize);
