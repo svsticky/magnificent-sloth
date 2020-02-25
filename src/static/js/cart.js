e96d09179bf0218297e3049a21c73449e7df87b7
Mstiekema <merijnstiekema@outlook.com>
2020-02-25 09:17:37 +0100

    Added comments and base for purchase function

--- src/static/js/cart.js
+++ src/static/js/cart.js
@@ -1,9 +1,11 @@
+const settings = require('../../../settings.json');
 let cartList = new Array();
 let cost = 0;
 
+// Puts the given product in the cart or increments it if
+// it's already present.
 module.exports.AddToCart = (product) => {
   let alreadyInCart = cartList.filter(prod => prod.id === product.id);
-  console.log(product.price)
   cost += Number(product.price);
   
   if(alreadyInCart.length !== 0) {
@@ -16,6 +18,9 @@ module.exports.AddToCart = (product) => {
   renderCart();
 }
 
+// Gets all the items from the cart and shows the product with amount
+// in the cart for the user to see. This function also increments the
+// total price which will be displayed for the user to see.
 function renderCart() {
   document.getElementById('cartList').innerHTML = '';
   for(let i = 0; i < cartList.length; i++) {
@@ -25,5 +30,35 @@ function renderCart() {
   }
   document.getElementById('totalCost').innerHTML = `€${Number(cost).toFixed(2)}`;
 }
+
+// Event for when the user click purchase. Send a request to Koala
+// to handle the payment for us.
+function purchase() {
+  let userInfo = {
+    token: settings.token,
+    uuid: '800a3bc7', // Test ID
+    items: cartList
+  }
+
+  const request = net.request({
+    method: 'POST',
+    hostname: settings.host,
+    port: settings.port,
+    path: 'api/checkout/transaction'
+  });
+
+  request.on('response', (response) => {
+    response.on('data', data => {
+      let bufferData = Buffer.from(data);
+      let res = bufferData.toString();
+      console.log(res);
+    });
+  });
+  
+  request.end(userInfo);
+}
+
+document.getElementById('purchase')
+  .addEventListener('click', () => purchase)
 
 renderCart();
