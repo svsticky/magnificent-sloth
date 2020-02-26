312722f63c505854659150d541f3eaef8044ce94
Nireon <siemvdtweel@gmail.com>
2020-02-26 12:59:32 +0100

    First functional version NFC

--- main.js
+++ main.js
@@ -4,41 +4,42 @@ const { NFC } = require('nfc-pcsc');
 
 
 function initialize () {
-const nfc = new NFC();
-console.log("nfc start")
-nfc.on('reader', reader => {
-
-	console.log(reader.name + ' reader attached, waiting for cards ...');
-
-	reader.on('card', card => {
-		console.log(card.uid);
+	const nfc = new NFC();
+	console.log("nfc start")
+	nfc.on('reader', reader => {
+	
+		console.log(reader.name + ' reader attached, waiting for cards ...');
+	
+		reader.on('card', card => {
+			console.log(card.uid);
+		});
+	
+		reader.on('error', err => {
+			console.error('reader error', err);
+		});
+	
+		reader.on('end', () => {
+			console.log(reader.name + ' reader disconnected.');
+		});
 	});
-
-	reader.on('error', err => {
-		console.error('reader error', err);
+	
+	nfc.on('error', err => {
+		console.error(err);
 	});
-
-	reader.on('end', () => {
-		console.log(reader.name + ' reader disconnected.');
-	});
-});
-
-nfc.on('error', err => {
-	console.error(err);
-});
 
   createWindow()
 }
 function createWindow () {
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       nodeIntegration: true
     }
   });
   win.webContents.openDevTools();
   win.loadFile('src/views/home/home.html');
+  return
 }
 
 app.on('ready', initialize);
