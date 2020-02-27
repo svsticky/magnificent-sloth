8bd705d126334598f1c0b420eb07f43d8e0950c6
Mstiekema <merijnstiekema@outlook.com>
2020-02-27 14:17:03 +0100

    Products visible after switching pages

--- src/static/js/menu.js
+++ src/static/js/menu.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const products = require('./products.js');
+const cart = require('./cart.js');
+let url = "products";
 
 function renderHTML(page) {
   page = path.join(__dirname, page);
-
   const file = fs.readFileSync(page);
   document.getElementById('content').innerHTML = file;
 }
 
-document.querySelectorAll('.link').forEach((element, index) => {
+function renderHomePage() {
+  renderHTML(`../../views/products/products.html`);
+  products.GetProducts();
+  cart.renderCart();
+}
+
+document.querySelectorAll('.link').forEach((element) => {
   element.addEventListener('click', function(e) {
-    renderHTML(`../${element.id}/${element.id}.html`);
+    url = element.id
+    switch(url) {
+      case "funds":
+        renderHTML(`../../views/funds/funds.html`);
+        break;
+      case "activities":
+        renderHTML(`../../views/activities/activities.html`);
+        break;
+      default:
+        renderHomePage();
+        break;
+    }
   });
 });
 
-renderHTML('../../views/products/products.html');
+renderHomePage();
