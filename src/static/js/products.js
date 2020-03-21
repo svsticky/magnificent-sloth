cce676ba6a8dfae83796f2c2fefbf8df8bf48e52
Nireon <siemvdtweel@gmail.com>
2020-03-21 17:02:06 +0100

    Added recent items to product page

--- src/static/js/products.js
+++ src/static/js/products.js
@@ -6,12 +6,19 @@ const { AddToCart } = require('./cart.js');
 module.exports.GetProducts = () => {
   // Define the API request we have to do to get all the items
   // from Koala.
+
   ipcRenderer.send('request', {
     name: 'getProducts',
     type: 'GET',
     url: 'api/checkout/products',
     body: null
   });
+
+  ipcRenderer.send('request',{
+    name: 'recent',
+    type: 'GET',
+    url: 'api/checkout/recent?uuid=uidempty',
+  })
 }
 
 // When we receive the data from Koala, render these
@@ -27,9 +34,21 @@ ipcRenderer.on('getProducts', (event, arg) => {
       renderProduct(products[i]);
   }
 });
+ipcRenderer.on('recent', (event, arg) => {
+  if (arg.err !== null){
+    console.error(arg.err);
+    document.getElementById('recentList').innerHTML = "Something went wrong while requesting data from Koala. Please try again later."
+  } else{
+    let products = JSON.parse(arg.data);
+    if (products.length == 0) document.getElementById('recent').remove()
+    for(let i = 0; i< products.length; i++){
+      renderProduct(products[i],true)
+    }
+  }
+})
 
 // Renders the block for each product.
-function renderProduct(prod) {
+function renderProduct(prod, recent=false) {
   let page = path.join(__dirname, '../../views/products/product.html');
   let product = fs.readFileSync(page);
   let html = document.createElement('article');
@@ -41,8 +60,8 @@ function renderProduct(prod) {
   html.getElementsByClassName('price')[0].innerHTML = `€${Number(prod.price).toFixed(2)}`
   if(prod.image)
     html.getElementsByClassName('productImage')[0].src = prod.image
-  html.addEventListener("touchstart", () => AddToCart(prod));
-  html.addEventListener("click", () => AddToCart(prod));
+  html.addEventListener("touchstart", () => {AddToCart(prod)});
+  html.addEventListener("click", () => {AddToCart(prod)});
 
-  document.getElementById('productList').append(html);
+  document.getElementById(recent ? 'recentList' : 'productList').append(html);
 }
