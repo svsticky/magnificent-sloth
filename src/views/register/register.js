da1a4be2092abb147d9ee1dccaa68ea7715bac7b
Nireon <siemvdtweel@gmail.com>
2020-03-21 15:30:52 +0100

    Removed old register.js

--- src/views/register/register.js
+++
@@ -1,7 +0,0 @@
-const studentinput = document.querySelector(".studentinput")
-document.querySelectorAll('.key').forEach((element, index) => {
-    element.addEventListener('click', function(e) {
-        studentinput.value += element.value
-
-    });
-  });
